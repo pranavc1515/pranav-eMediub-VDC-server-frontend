a8c655599aa6b215021cec12adab2854334d8ff3
pranavc1515 <pranavc1515@gmail.com>
2025-06-29 15:47:59 +0530

    refactor(family): remove family-related components and configurations
    - Deleted FamilyService and associated components for managing family members, including forms and views.
    - Removed references to family members in navigation and localization files.
    - Cleaned up related routes and icons from the navigation configuration.

--- src/configs/routes.config/routes.config.ts
+++ src/configs/routes.config/routes.config.ts
@@ -98,12 +98,7 @@ export const protectedRoutes: Routes = [
         component: lazy(() => import('@/views/user/Settings')),
         authority: ['user'],
     },
-    {
-        key: 'userFamily',
-        path: '/user/family',
-        component: lazy(() => import('@/views/user/Family')),
-        authority: ['user'],
-    },
+
     /** Example purpose only, please remove */
     {
         key: 'singleMenuItem',
