29628012289d3132ca15fc69764daf5cb74c7abb
pranavc1515 <pranavc1515@gmail.com>
2025-07-13 23:15:14 +0530

    [UPDATE]: remove copyright and footer links from Footer component
    - Eliminated copyright notice and links to Privacy Policy and Terms & Conditions from the Footer component to simplify the footer content.
    - Adjusted layout to maintain visual consistency following the removal of these elements.

--- src/components/template/Footer.tsx
+++ src/components/template/Footer.tsx
@@ -13,28 +13,7 @@ type FooterProps = {
 const FooterContent = () => {
     return (
         <div className="flex items-center justify-between flex-auto w-full">
-            <span>
-                Copyright &copy; {`${new Date().getFullYear()}`}{' '}
-                <span className="font-semibold">{`${APP_NAME}`}</span> All
-                rights reserved.
-            </span>
-            <div className="">
-                <a
-                    className="text-gray"
-                    href="/#"
-                    onClick={(e) => e.preventDefault()}
-                >
-                    Term & Conditions
-                </a>
-                <span className="mx-2 text-muted"> | </span>
-                <a
-                    className="text-gray"
-                    href="/#"
-                    onClick={(e) => e.preventDefault()}
-                >
-                    Privacy & Policy
-                </a>
-            </div>
+            
         </div>
     )
 }
